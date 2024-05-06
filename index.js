bebfa8dbfcb04d3a3d894dd265c6eff030c8e472
Leonardo Schmitt <leoeyeschmittk@gmail.com>
2024-05-06 12:11:18 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,83 @@
+class Sorteador {
+  equipes;
+  numeroEquipes;
+  pessoas;
+
+  constructor(pessoas, numeroEquipes) {
+    this.setPessoas(pessoas);
+    this.setNumeroEquipes(numeroEquipes);
+    this.sortear();
+  }
+
+  sortear() {
+    const equipes = [];
+    const pessoas = this.getPessoas();
+    const quantidadePessoas = pessoas.length;
+    const divisor = quantidadePessoas / this.getNumeroEquipes();
+    const numeroEquipes = this.getNumeroEquipes();
+    let equipe = [];
+
+    function x() {
+      let randomN = Math.floor(Math.random() * pessoas.length);
+      equipe.push(pessoas[randomN]);
+      pessoas.splice(randomN, 1);
+      if (pessoas.length % divisor === 0 || pessoas.length === 0) {
+        equipes.push(equipe);
+        equipe = [];
+      }
+      if (pessoas.length >= 1) {
+        return x();
+      } else {
+        return equipes;
+      }
+    }
+    this.setEquipes(x());
+    return;
+  }
+
+  setPessoas(pessoas) {
+    this.pessoas = pessoas;
+  }
+
+  getPessoas() {
+    return this.pessoas;
+  }
+
+  setNumeroEquipes(numeroEquipes) {
+    this.numeroEquipes = numeroEquipes;
+  }
+
+  getNumeroEquipes() {
+    return this.numeroEquipes;
+  }
+
+  setEquipes(equipes) {
+    this.equipes = equipes;
+  }
+
+  getEquipes() {
+    return this.equipes;
+  }
+
+  printEquipes() {
+    const equipes = this.getEquipes();
+
+    for (let i = 0; i < equipes.length; ++i) {
+      console.log(`Equipe ${i + 1}: ${equipes[i].join(", ")}`);
+    }
+  }
+}
+
+const sorteador = new Sorteador(
+  [
+    "leo",
+    "dudu",
+    "israel",
+    "henrique",
+    "gustavo",
+    "marcos",
+    "gabriel",
+    "gabriel II",
+  ],
+  4
+).printEquipes();
